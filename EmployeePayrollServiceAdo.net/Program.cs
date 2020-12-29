610112d70e7bb358e447e56501299ff2a7ce8e51
Vidyadhar1997 <vidyadharhudge1997@gmail.com>
2020-12-29 18:12:02 +0530

    [vidyadhar hudge]Add:Welcome message

---
+++ EmployeePayrollServiceAdo.net/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace EmployeePayrollServiceAdo.net
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcomr to the Employee payroll service ado .net program");
+            
+        }
+    }
+}
